d3f3c128a37bb4eb10121cb26f7f073b1513bcfe
stevecreated@gmail.com <stevecreated@gmail.com>
2017-06-28 10:55:30 -0700

    mapdispatchtoprops added

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,4 +1,6 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
+import * as courseActions from '../../actions/courseActions'
 
 class CoursesPage extends Component {
   constructor(props, context) {
@@ -19,13 +21,19 @@ class CoursesPage extends Component {
   }
 
   onClickSave() {
-    alert(`Saving ${this.state.course.title}`)
+    this.props.createCourse(this.state.course)
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>
   }
 
   render() {
+
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input
           type="text"
@@ -40,4 +48,25 @@ class CoursesPage extends Component {
   }
 }
 
-export default CoursesPage
+CoursesPage.propTypes = {
+  createCourse: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+}
+
+function mapStateToProps(state, ownProps) {
+
+  return {
+    courses: state.courses
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  }
+}
+
+// const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps)
+// export default connectedStateAndProps(CoursesPage)
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage)
